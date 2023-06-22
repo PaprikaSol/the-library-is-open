1e6ec8acd4e0471bf44375ea9dcfb5aa1d1d90ee
PaprikaSol <zoipapajim@gmail.com>
2023-06-22 17:06:11 +0300

    Clean-up

--- app/src/main/java/com/example/libraryapp/list/BooksListFragment.kt
+++ app/src/main/java/com/example/libraryapp/list/BooksListFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.libraryapp.R
@@ -37,9 +39,11 @@ class BooksListFragment : Fragment(R.layout.fragment_books_list),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch {
-            viewModel.getPagerFlow().collectLatest { pagingData ->
-                booksAdapter.submitData(lifecycle, pagingData)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.getPagerFlow().collectLatest { pagingData ->
+                    booksAdapter.submitData(lifecycle, pagingData)
+                }
             }
         }
     }
